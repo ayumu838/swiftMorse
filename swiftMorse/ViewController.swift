0a2d3381dacf7b1fa4477f38ed0d79102c4dcc4d
Ayumu <ayumu839.a@gmail.com>
2017-07-05 23:41:32 +0900

    Initial Commit

---
+++ swiftMorse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftMorse
+//
+//  Created by 望月歩 on 2017/07/05.
+//  Copyright © 2017年 望月歩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
